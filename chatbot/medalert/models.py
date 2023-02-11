f528fef2ac24445a1968ccdd788abaacdb17874a
aryavardhanjain <117517293+aryavardhanjain@users.noreply.github.com>
2023-02-11 18:46:31 +0530

    Update models.py
    isme bhi honge aur changes

--- chatbot/medalert/models.py
+++ chatbot/medalert/models.py
@@ -5,4 +5,12 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
-    
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    content = models.TextField()
+    email = models.EmailField()
+    id  = models.IntegerField
+
+    def __str__(self):
+        return 'Message from ' + self.name + ' - ' + self.email
