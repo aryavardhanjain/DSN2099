ffe1cbe166fd535d0e03cc7d363f1acaa57c3a61
aryavardhanjain <117517293+aryavardhanjain@users.noreply.github.com>
2023-02-11 18:45:30 +0530

    Update views.py
    isme aur changes honge abhi

--- chatbot/medalert/views.py
+++ chatbot/medalert/views.py
@@ -8,17 +8,56 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .models import User
+from .models import User, Contact
 
 # Create your views here.
 
 def index(request):
     return render(request, "medalert/index.html")
 
+def about(request):
+    return render(request, "medalert/about.html")
+
+def contact(request):
+    if request.method=="POST":
+        contact = Contact()
+        user_name = request.POST["name"]        
+        email_address = request.POST["email"]
+        content = request.POST["content"]
+        contact.name = user_name
+        contact.email = email_address
+        contact.content = content
+        contact.save()
+        return HttpResponse("<h1> THANK YOU FOR CONTACTING US</h1>")
+    
+    return render(request, "medalert/contact.html")
+
+def login_view(request):
+     if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "medalert/login.html", {
+                "message": "Invalid username and/or password."
+            })
+     
+     else:
+        return render(request, "medalert/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
 def register(request):
     if request.method == "POST":
         username = request.POST["username"]
-        email = request.POST["email"]
 
         # Ensure password matches confirmation
         password = request.POST["password"]
@@ -31,41 +70,15 @@ def register(request):
 
         # Attempt to create new user
         try:
-            newuser = User.objects.create_user(username, email, password)
-            return render(request, "medalert/register1.html",
-            {
-                "message": "Registration Successful"
-            })
-            newuser.save()
+            user = User.objects.create_user(username, password)
+            user.save()
         except IntegrityError:
             return render(request, "medalert/register.html",
             {
                 "message": "Username already taken."
             })
-        login(request, newuser)
-        return HttpResponseRedirect(reverse("login"))
+        login(request,user)
+        return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, 'medalert/register.html')
 
-def login_view(request):
-    if request.method == "POST":
-
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-
-        # Check if authentication is succesful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "medalert/login.html", {
-                "message": "Invalid username/password"
-            })
-    else:
-        return render(request, "medalert/login.html")
-
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
