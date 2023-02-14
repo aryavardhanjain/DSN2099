6c2dde1704f4eddd6b229a6c2c8d00d032b32fa9
aryavardhanjain <117517293+aryavardhanjain@users.noreply.github.com>
2023-02-14 13:27:56 +0530

    Update views.py

--- chatbot/medalert/views.py
+++ chatbot/medalert/views.py
@@ -18,42 +18,30 @@ def index(request):
 def about(request):
     return render(request, "medalert/about.html")
 
+def services(request):
+    return render(request, "medalert/services.html")
+
 def contact(request):
     if request.method=="POST":
         contact = Contact()
         user_name = request.POST["name"]        
         email_address = request.POST["email"]
         content = request.POST["content"]
-        contact.name = user_name
-        contact.email = email_address
-        contact.content = content
-        contact.save()
-        return HttpResponse("<h1> THANK YOU FOR CONTACTING US</h1>")
-    
-    return render(request, "medalert/contact.html")
-
-def login_view(request):
-     if request.method == "POST":
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "medalert/login.html", {
-                "message": "Invalid username and/or password."
+        if len(user_name)<2 or len(email_address)<3 or len(content)<5:
+            return render(request, "medalert/contact.html", {
+                "message": "Invalid Form Submission."
             })
-     
-     else:
-        return render(request, "medalert/login.html")
-
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
+        else:
+            contact.name = user_name
+            contact.email = email_address
+            contact.content = content
+            contact.save()
+            return render(request, "medalert/contact.html", 
+            {
+                "message1": "Thank You for Contacting Us. We Will Get Back to You Shortly."
+            })
+    else:
+        return render(request, "medalert/contact.html")
 
 def register(request):
     if request.method == "POST":
@@ -81,4 +69,29 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, 'medalert/register.html')
+
+
+def login_view(request):
+     if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "medalert/login.html", {
+                "message": "Invalid username and/or password."
+            })
+     
+     else:
+        return render(request, "medalert/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
 
