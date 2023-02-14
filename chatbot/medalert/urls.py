931471ef6b3f622ccf6e1dbd7df61e6f9414a570
aryavardhanjain <117517293+aryavardhanjain@users.noreply.github.com>
2023-02-14 13:27:32 +0530

    Update urls.py

--- chatbot/medalert/urls.py
+++ chatbot/medalert/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout/', views.logout_view, name="logout"),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
 ]
